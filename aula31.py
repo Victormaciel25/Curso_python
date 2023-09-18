b3ffaf94acb91f54e98637ea3e9a7111b36d9c74
Victor Maciel <victormaciel.dev@gmail.com>
2023-09-18 15:11:37 -0300

    update

---
+++ aula31.py
@@ -0,0 +1,19 @@
+"""
+Flag (Bandeira) - Marcar um local
+None = Não valor
+Is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+
+condicao = False
+passou_no_if = None
+
+if condicao:
+    print('Faça algo')
+else:
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
