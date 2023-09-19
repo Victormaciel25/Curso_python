1c9f8e81037badccfd2836644540aa34ed8e42f2
Victor Maciel <victormaciel.dev@gmail.com>
2023-09-19 17:35:51 -0300

    Update aula34.py

--- aula34.py
+++ aula34.py
@@ -2,5 +2,12 @@
 Repetições
 while (enquanto)
 Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim.
 """
 
+condicao = True
+
+while condicao:
+    print(1)
+    print(2)
+    print(3)
