2dacade087475162f856848e447bfcd21a4b0f38
Victor Maciel <victormaciel.dev@gmail.com>
2023-09-21 01:02:22 -0300

    Update aula34.py

--- aula34.py
+++ aula34.py
@@ -8,6 +8,10 @@ Loop infinito -> Quando um código não tem fim.
 condicao = True
 
 while condicao:
-    print(1)
-    print(2)
-    print(3)
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou')
