d499b9a95ee04992405dbf75f30d5143c3e647fa
Victor Maciel <victormaciel.dev@gmail.com>
2023-10-04 23:44:53 -0300

    update

---
+++ aula48,1.py
@@ -0,0 +1,25 @@
+"""
+Lista em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: 
+append, insert, pop, del, clear, extend, +
+Create Read Update Delete
+Criar , ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+#         0   1   2   3
+lista = [10, 20, 30, 40]
+lista[2] = 300
+del lista[2]
+print(lista)
+print(lista[2])
+
+lista.append(50)
+lista.pop() # remove o elemento anterior
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop()
+print(lista, 'Removido:', ultimo_valor)
+
---
+++ aula48.py
@@ -0,0 +1,21 @@
+"""
+Lista em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#       +01234
+#       -54321
+string = 'ABCDE' # 5 caracteres
+
+# print(lista, type(lista))
+
+#         0     1           2         3    4
+#        -5    -4          -3        -2   -1
+lista = [123, True, 'Victor Maciel', 1.2, []]
+lista[-3] = 'Jhulya'
+print(lista)
+print(lista[2].upper(), type(lista[2]))
+
