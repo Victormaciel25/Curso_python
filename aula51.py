ba31084ac0c6378c465bf9872fcd688736da65b3
Victor Maciel <victormaciel.dev@gmail.com>
2023-12-03 00:44:58 -0300

    studing

---
+++ aula51.py
@@ -0,0 +1,6 @@
+"""
+Introdução ao desempacotamento + tuples (tuplas)
+"""
+_, _, nome3, *resto = ['Maria', 'Helena', 'Luiz']
+print(nome3)
+
---
+++ aula52.py
@@ -0,0 +1,7 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+nomes = 'Maria', 'Helena', 'Luiz'
+# nomes = tuple(nomes)    - Se quiser converter uma lista para tupla
+# nomes = list(nomes)    - Se quiser converter uma tupla para lista
+print(nomes[0])
