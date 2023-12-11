c364e76bee8698e80bdd4ad2eda84eca55782bb0
Victor Maciel <victormaciel.dev@gmail.com>
2023-12-11 16:08:31 -0300

    Update aula56.py

--- aula56.py
+++ aula56.py
@@ -4,5 +4,12 @@ split - divide uma string
 join - une uma string
 """
 frase = 'Olha só que, coisa interessante'
-lista_palavras = frase.split()
-print(lista_palavras)
+lista_frases_cruas = frase.split(',')
+
+lista_frases = []
+
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+frases_unidas = ','.join(lista_frases)
+print(frases_unidas) 
