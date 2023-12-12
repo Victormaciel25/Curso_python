8e3afb641042e00d4c666f887cade234862b021e
Victor Maciel <victormaciel.dev@gmail.com>
2023-12-12 16:55:18 -0300

    update

--- aula56.py
+++ aula56.py
@@ -12,4 +12,4 @@ for i, frase in enumerate(lista_frases_cruas):
     lista_frases.append(lista_frases_cruas[i].strip())
 
 frases_unidas = ','.join(lista_frases)
-print(frases_unidas) 
+print(lista_frases) 
---
+++ aula58.py
@@ -0,0 +1,13 @@
+"""
+Interpretador do Python
+
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c 'cmd' (comando)
+python -i mod.py (interativo com mod)
+
+The Zen of Python, por Tim Peters
+
+Bonito é melhor...
+"""
