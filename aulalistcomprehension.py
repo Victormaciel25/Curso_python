7641352cf1bfeb58d21086a4b35346c04866ff11
Victor Maciel <victormaciel.dev@gmail.com>
2024-01-04 12:01:41 -0300

    Create aulalistcomprehension.py

---
+++ aulalistcomprehension.py
@@ -0,0 +1,9 @@
+numeros = [1, 2, 3, 4, 5]
+divisão = [numero / 2 for numero in numeros]
+multiplicação = [numero * 2 for numero in numeros]
+quadrado = [numero ** 2 for numero in numeros]
+
+print(numeros)
+print(divisão)
+print(multiplicação)
+print(quadrado)
