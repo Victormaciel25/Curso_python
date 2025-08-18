b974312e5deab77c4dc97c4a192a5a2430e8cf57
Victor Maciel <victormaciel.dev@gmail.com>
2025-08-18 13:13:38 -0300

    Update isinstace87.py

--- isinstace87.py
+++ isinstace87.py
@@ -1,2 +1,7 @@
 # isinstace - para se o objeto é de determinado tipo
-lista = ['a', 1, 1.1, True, [1, 2, 3], (1, 2), {0, 1}, {'nome': 'Luiz'}]
+lista = [
+    'a', 1, 1.1, True, [1, 2, 3], (1, 2), {0, 1}, {'nome': 'Luiz'}
+]
+
+for item in lista:
+    print(item, isinstance(item, set))
